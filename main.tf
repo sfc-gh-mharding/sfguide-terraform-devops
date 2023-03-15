8b569c9f7c1b33798a216abf0bec1ebaeb2e56c6
Michel Harding <michel.harding@snowflake.com>
2023-03-15 16:58:04 -0700

    Changed colum names

--- main.tf
+++ main.tf
@@ -23,39 +23,33 @@ resource "snowflake_database" "tf_devops_demo_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "schema" {
-  database            = "TF_DEVOPS_DEMO_DB"
+resource "snowflake_schema" "demo" {
+  database            = snowflake_database.tf_devops_demo_db.name
   name                = "DEMO"
   data_retention_days = 1
 }
 
-resource "snowflake_table" "table" {
+resource "snowflake_table" "tab1" {
   database            = snowflake_schema.schema.database
   schema              = snowflake_schema.schema.name
-  name                = "table"
+  name                = "TABLE1"
   comment             = "A table."
   cluster_by          = ["to_date(DATE)"]
   data_retention_days = snowflake_schema.schema.data_retention_days
   change_tracking     = false
 
   column {
-    name     = "id"
+    name     = "ID"
     type     = "int"
     nullable = true
 
   }
-
+  
   column {
-    name     = "identity"
-    type     = "NUMBER(38,0)"
+    name = "FIRST_NAME"
+    type = "VARCHAR"
     nullable = true
-
-    identity {
-      start_num = 1
-      step_num  = 3
-    }
   }
-
 
   column {
     name = "DATE"
@@ -63,13 +57,13 @@ resource "snowflake_table" "table" {
   }
 
   column {
-    name    = "extra"
+    name    = "V"
     type    = "VARIANT"
-    comment = "extra data"
+    comment = "A column of Variant type"
   }
 
   primary_key {
-    name = "my_key"
-    keys = ["data"]
+    name = "TAB1_KEY"
+    keys = ["ID"]
   }
 }

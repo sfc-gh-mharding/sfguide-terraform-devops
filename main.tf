b701ff1fc8c8cd8023d5a56c6de61d2481fe74a0
Michel Harding <michel.harding@snowflake.com>
2023-03-15 10:58:45 -0700

    First run

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.35"
+    }
+  }
+
+backend "remote" {
+    organization = "mhardingsnow"
+
+    workspaces {
+      name = "github-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "tf_devops_demo_db" {
+  name    = "TF_DEVOPS_DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
